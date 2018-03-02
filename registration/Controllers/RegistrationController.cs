0c89d4eab3d4773f25ec817a816f26f79c5fbfa4
Beth Sheldon <beth.sheldon@moneycorp.com>
2018-03-02 11:53:41 +0000

    add pwned password k anonymity check ot password validation

--- registration/Controllers/RegistrationController.cs
+++ registration/Controllers/RegistrationController.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using registration.Models;
 
@@ -15,15 +21,52 @@ namespace registration.Controllers
         }
         
         [HttpPost]
-        public IActionResult Validate(string username, string password)
+        public async Task<IActionResult> Validate(string username, string password)
         {
             var result = Zxcvbn.MatchPassword(password, new List<string>{username});
-            return View("Index", new RegistrationViewModel {PasswordStrengthResult = result});
+            var count = await PwnedPasswordCheck(password);
+            return View("Index", new RegistrationViewModel {PasswordStrengthResult = result, BreachCount = count});
         }
 
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static async Task<int?> PwnedPasswordCheck(string password)
+        {
+            var hashedPassword = Hash(password);
+            var hash5 = hashedPassword.Substring(0, 5);
+            var handler = new HttpClientHandler {UseProxy = false};
+            var client = new HttpClient(handler) {BaseAddress = new Uri("https://api.pwnedpasswords.com")};
+            var result = await client.GetAsync($"/range/{hash5}");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                var hashSet = content.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+                var relevantResult = hashSet.SingleOrDefault(h => hash5 + h.Split(":")[0] == hashedPassword);
+                return relevantResult == null ? (int?) null : int.Parse(relevantResult.Split(":")[1]);
+            }
+
+            return null;
+        }
+
+        private static string Hash(string input)
+        {
+            using (var sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (var b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
         }
     }
 }
--- registration/Models/RegistrationViewModel.cs
+++ registration/Models/RegistrationViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace registration.Models
+﻿using Zxcvbn;
+
+namespace registration.Models
 {
     public class RegistrationViewModel
     {
-        public Zxcvbn.Result PasswordStrengthResult { get; set; }
+        public Result PasswordStrengthResult { get; set; }
+        public int? BreachCount { get; set; }
     }
 }
